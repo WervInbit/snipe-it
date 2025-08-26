8ae56feff5b1c063f079aafab27fb2cd204ec875
Avhaari <joeymarcune@gmail.com>
2025-08-26 13:01:55 +0200

    Add test run framework

---
+++ database/factories/TestTypeFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TestType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class TestTypeFactory extends Factory
+{
+    protected $model = TestType::class;
+
+    public function definition()
+    {
+        $name = $this->faker->words(2, true);
+
+        return [
+            'name' => ucfirst($name),
+            'slug' => Str::slug($name) . '-' . Str::random(5),
+            'tooltip' => $this->faker->sentence(),
+        ];
+    }
+}
