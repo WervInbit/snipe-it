a5f53a903807b179c1089e74106b48aa44e1c2ec
Avhaari <joeymarcune@gmail.com>
2025-09-12 01:44:25 +0200

    Add alternative QR label templates

--- config/qr_templates.php
+++ config/qr_templates.php
@@ -8,5 +8,17 @@ return [
             'height_mm' => 36,
             'qr_size' => 200,
         ],
+        'dymo-54x25' => [
+            'name' => 'Dymo 54×25mm',
+            'width_mm' => 54,
+            'height_mm' => 25,
+            'qr_size' => 150,
+        ],
+        'square-50' => [
+            'name' => 'Square 50mm',
+            'width_mm' => 50,
+            'height_mm' => 50,
+            'qr_size' => 200,
+        ],
     ],
 ];
